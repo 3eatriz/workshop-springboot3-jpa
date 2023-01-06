2282fca83687da76ee7be911d3066273c6e6c0ff
Beatriz <beatrizfreitasti@gmail.com>
2023-01-06 12:26:16 -0300

    Categoria

---
+++ src/main/java/com/estudos/beatriz/curse/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.estudos.beatriz.curse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudos.beatriz.curse.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
